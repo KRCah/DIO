3388e381cb2a4daad000c4e35ae35eeb158ba00a
KRCah <camilaaokr@gmail.com>
2022-02-11 20:08:39 -0300

    "criando Class Livrinho"

--- Bootcamp/IDEs JAVA/Intellij/src/br/com/dio/PrimeiroPrograma.java
+++ Bootcamp/IDEs JAVA/Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -12,6 +12,45 @@ public class PrimeiroPrograma
         /*int a=10;
         int b=2;
         System.out.println("Hello World" + (a*b));*/
+
+        Livro livrinho = new Livro("Aprendendo Programação", 300);
+        System.out.println(livrinho);
     }
 
+}
+
+class Livro
+{
+    private String nome;
+    private int numPaginas;
+
+    public Livro(String nome, int numPaginas)
+    {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
