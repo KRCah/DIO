a30384fe4f40ec7d901f359687730b056b0513be
KRCah <camilaaokr@gmail.com>
2022-02-11 11:12:13 -0300

    "First commit - Dominando IDEs Java"

---
+++ Bootcamp/Eclipse/PrimeiroPrograma/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.gato.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) 
+	{
+		Gato gato = new Gato("Blue","Siames",2);
+		System.out.println(gato);
+	//*******************************************************************
+		int a= gato.getIdade();
+		String nome = gato.getNome();
+		int b=2;
+
+		System.out.println("Hello World, " + nome +" have "+ a +" years old");
+
+	}
+
+}
---
+++ Bootcamp/IDEs JAVA/PrimeiraAula/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class PrimeiroPrograma
+{
+   public static void main(String args[])
+   {
+        int a= 10;
+        int b= 20;
+        int soma= a+b;
+        System.out.println("Hello World" + " "+soma);
+   }
+}
